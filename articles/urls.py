08553538ff3e11865e3613845aa1422f54aba6e7
armut <abdullah.akalin@gazi.edu.tr>
2016-08-31 17:36:01 +0300

    Login form redesigned.

--- articles/urls.py
+++ articles/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^login$', views.loginView, name ='login'),
     url(r'^logout$', views.logoutView, name ='logout'),
 
+    url(r'^user/(?P<pk>\d+)/$', views.UserView.as_view(), name='user'),
     url(r'^category/(?P<slug>[-\w]+)/$', views.CategoryView.as_view(), name='category'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', views.PostView.as_view(), name='post'),
-    url(r'^user/(?P<pk>)/$', views.UserView.as_view(), name='user')
 ]
