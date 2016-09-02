7d8d991730031e9a7620c21cd22c20417e774e35
armut <abdullah.akalin@gazi.edu.tr>
2016-09-02 15:30:20 +0300

    Created authors groups on django admin.

--- articles/admin.py
+++ articles/admin.py
@@ -1,14 +1,35 @@
 from django.contrib import admin
 from .models import Post, Comment, Genre, Whoami
 
-class PostAdmin(admin.ModelAdmin):
+class FilterUsers(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+    def get_queryset(self, request):
+        qs = super(FilterUsers, self).get_queryset(request)
+        if not request.user.is_superuser:
+            return qs.filter(author=request.user)
+        else:
+            return qs
+
+    def has_change_permission(self, request, obj=None):
+        if not obj or request.user.is_superuser:
+            return True
+        return obj.author == request.user
+
+class PostAdmin(FilterUsers):
     prepopulated_fields = { 'slug': ('title',) }
 
 class GenreAdmin(admin.ModelAdmin):
     prepopulated_fields = { 'slug': ('name',)  }
 
+class WhoamiAdmin(FilterUsers):
+    pass
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Genre, GenreAdmin)
-admin.site.register(Whoami)
+admin.site.register(Whoami, WhoamiAdmin)
 
